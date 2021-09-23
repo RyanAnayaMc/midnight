24656952d4c6f0d13685a14159abba57b10a41c5
『 Night  ♪ 』 <ryanmcintosh62@icloud.com>
2021-09-22 22:48:23 -0700

    added audio core and weightedlist
    Ported audio core from Night Bot:
    AudioPlayerSendHandler
    GuildMusicManager
    PlayerManager
    TrackScheduler
    
    Finished WeightedList class

--- src/main/java/me/night/midnight/midnight_bot/audio/IntroDetail.java
+++ src/main/java/me/night/midnight/midnight_bot/audio/IntroDetail.java
@@ -3,20 +3,31 @@ package me.night.midnight.midnight_bot.audio;
 import java.util.Arrays;
 
 public class IntroDetail {
-	private String filename;
-	private String fileDirectory;
+	private String path;
 	private int volume;
 	private ExtraAction[] extraActions;
+	private boolean adminOnly;
 	
-	public IntroDetail(String dir, String fName, int vol, ExtraAction...actions) {
-		filename = fName;
-		fileDirectory = dir;
+	/**
+	 * Creates a new IntroDetail
+	 * @param p The path to the audio file
+	 * @param vol The volume for the sound
+	 * @param admin Whether or not this IntroDetail can only be changed by admins
+	 * @param actions Any extra actions that will occur
+	 */
+	public IntroDetail(String p, int vol, boolean admin, ExtraAction...actions) {
+		path = p;
 		volume = Math.max(0, vol);
 		extraActions = actions;
+		adminOnly = admin;
+	}
+	
+	public boolean isAdminOnly() {
+		return adminOnly;
 	}
 	
 	public String getPath() {
-		return fileDirectory + filename;
+		return path;
 	}
 	
 	public int getVolume() {
